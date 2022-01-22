9fe1413ca9e3934e70f86efcd4f23ff87f2bd0f1
hbedford <36144175+hbedford@users.noreply.github.com>
2022-01-22 19:58:23 -0300

    add screen info

--- lib/src/screens/lobby/lobby_screen.dart
+++ lib/src/screens/lobby/lobby_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:war/src/views/lobby/lobby_view.dart';
 
 class LobbyScreen extends StatelessWidget {
   const LobbyScreen({Key? key}) : super(key: key);
@@ -6,7 +7,7 @@ class LobbyScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: LobbyScreen(),
+      body: LobbyView(),
     );
   }
 }
